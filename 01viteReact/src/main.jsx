4974d8a5d38789d54b07392d429a5915421e6865
thecodephilic-guy <sohailansari483@gmail.com>
2023-10-25 01:52:48 +0530

    made a custom element in main.jsx

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//custom written code that is to be rendered
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'Click to visit goolge.com'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  <App />
+
 )
